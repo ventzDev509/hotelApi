f97770ca46137e5d708a25e47da80941861db8f4
ventzDev509 <156130198+ventzDev509@users.noreply.github.com>
2024-01-09 12:39:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const cors = require('cors')
 const { required } = require('joi')
 const app = express()
-const PORT = 3000
+const PORT = 5000
 sequelize.initDb()
 app.use(cors())
 app.use(bodyParser.json());
