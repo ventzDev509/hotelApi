5e4ff4755fc10c2602f6dd4ba4ea4833325f716c
ventzDev509 <156130198+ventzDev509@users.noreply.github.com>
2024-01-09 12:13:25 -0800

    Add files via upload

---
+++ db/sequelize.js
@@ -0,0 +1,29 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const User = require("../models/User");
+const CHAMBRE=require('../models/chambre')
+const RESERVATION=require('../models/Reservation')
+require("dotenv").config()
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DB_USERNAME, process.env.DBPASSWORD, {
+  host: process.env.DB_HOST,
+  dialect:"mysql",
+});
+
+sequelize 
+  .authenticate()
+  .then((_) => "connexion reussir")
+  .catch((error) => console.log(error));
+
+const user = User(sequelize, DataTypes);
+const reservation=RESERVATION(sequelize,DataTypes)
+const chambre=CHAMBRE(sequelize,DataTypes)
+
+const initDb = () => {
+  return sequelize.sync().then((_) => { }); 
+}; 
+
+module.exports = {
+  initDb,
+  user, 
+  reservation,
+  chambre,
+};
