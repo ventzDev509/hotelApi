dcdb7485244b42d43a45b995f01c90c97b8851da
ventzDev509 <156130198+ventzDev509@users.noreply.github.com>
2024-01-11 13:08:18 -0800

    Add files via upload

--- route/Reservation/add.js
+++ route/Reservation/add.js
@@ -2,7 +2,7 @@ const { reservation } = require('../../db/sequelize')
 const { chambre } = require("../../db/sequelize")
 const addReservationV = require('../../validationDatas/reservationValidation')
 module.exports = (app) => {
-    app.post('/api/reservation/new', (req, res) => {
+    app.post('/api/reservation/new/:code', (req, res) => {
         const date = new Date();
         const dateUserDebut = new Date(req.body.dateDebut);
         const dateFin = new Date(req.body.dateFin)
@@ -10,6 +10,7 @@ module.exports = (app) => {
 
         const { error } = addReservationV(req.body).addReservationV
         if (error) return res.status(501).json(error.details[0].message)
+
         reservation.findOne({
             where: {
                 codeChambre: req.body.codeChambre,
@@ -24,37 +25,42 @@ module.exports = (app) => {
                 } else {
                     chambre.findOne({ where: { codeChambre: req.body.codeChambre } })
                         .then(response => {
-                            if (req.body.NumberOfPerson < 1) {
-                                const msg = `La chambre doit avoir au moin une personne`
-                                return res.json({ msg })
-                            } else {
-                                if (response.numberOfPersons < req.body.NumberOfPerson) {
-                                    const msg = `La chambre peut avoir ${response.numberOfPersons} personnes pas plus`
+                            if (response != null) {
+
+
+                                if (req.body.NumberOfPerson < 1) {
+                                    const msg = `La chambre doit avoir au moin une personne`
                                     return res.json({ msg })
                                 } else {
-                                    if (dateUserDebut > currentDate) {
-                                        if (dateUserDebut > dateFin) {
-                                            const msg = "La dete fin du reservation doit etre supperieur a la date debut"
-                                            return res.json({ msg })
-                                        } else {
-                                            reservation.create(req.body)
-                                                .then(response => {
-                                                    const msg = "Votre Reservation a ete effectue avec success"
-                                                    res.json({ msg, response })
-                                                    const data = {
-                                                        "status": "occupe"
-                                                    }
-                                                    chambre.update({ ...data }, { where: { codeChambre: response.codeChambre } })
-                                                })
-                                                .catch(error => res.json(error))
-                                        }
+                                    if (response.numberOfPersons < req.body.NumberOfPerson) {
+                                        const msg = `La chambre peut avoir ${response.numberOfPersons} personnes pas plus`
+                                        return res.json({ msg })
                                     } else {
-                                        return res.json({ msg: "Vous ne pouvez pas faire une reservation pour une date passe" })
+                                        if (dateUserDebut > currentDate) {
+                                            if (dateUserDebut > dateFin) {
+                                                const msg = "La dete fin du reservation doit etre supperieur a la date debut"
+                                                return res.json({ msg })
+                                            } else {
+                                                reservation.create(req.body)
+                                                    .then(response => {
+                                                        const msg = "Votre Reservation a ete effectue avec success"
+                                                        res.json({ msg, response })
+                                                        const data = {
+                                                            "status": "occupe"
+                                                        }
+                                                        chambre.update({ ...data }, { where: { codeChambre: response.codeChambre } })
+                                                    })
+                                                    .catch(error => res.json(error))
+                                            }
+                                        } else {
+                                            return res.json({ msg: "Vous ne pouvez pas faire une reservation pour une date passe" })
+                                        }
+
                                     }
-
                                 }
+                            }else{
+                                res.status(404).json({msg:"Aucune chambre n'a ete trouver"})
                             }
-
                         })
 
                 }
