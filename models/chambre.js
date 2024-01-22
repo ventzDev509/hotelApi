4c3434316a85eac22cefb078920dfd78fa29ea32
ventzDev509 <156130198+ventzDev509@users.noreply.github.com>
2024-01-22 09:33:14 -0800

    Add files via upload

--- models/chambre.js
+++ models/chambre.js
@@ -38,6 +38,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      admin:{
+        type:DataTypes.STRING(100),
+        allowNull:false
+      }
     },
     {
       timestamps: true,
