c797cbdb9aca38c0b5d2d49d9d1610940850747f
ventzDev509 <156130198+ventzDev509@users.noreply.github.com>
2024-01-22 18:02:15 -0800

    Add files via upload

--- route/gestionChambre/update.js
+++ route/gestionChambre/update.js
@@ -9,28 +9,14 @@ const updateValidation = require('../../validationDatas/roomValidation');
 const multer = require("multer");
 const path = require("path");
 
-// Configuring Multer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        // Specify the destination folder for uploaded files
-        cb(null, "image");
-    },
-    filename: (req, file, cb) => {
-        // Use a unique filename to avoid conflicts
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
-    },
-});
-const upload = multer({ storage: storage });
 
 // Exporting the route handling function
 module.exports = (app) => {
     // Define a POST route for updating room information
-    app.post("/api/room/update/:idChambre", upload.single("image"), (req, res) => {
+    app.post("/api/room/update/:idChambre", (req, res) => {
         // Extract the room ID from the endpoint parameters
         const id = req.params.idChambre;
-        // Extract the image buffer from the uploaded file
-        const imageBuffer = req.file;
+
         // Validate the received data
         const dataValidate = {
             "roomType": req.body.roomType,
@@ -45,34 +31,16 @@ module.exports = (app) => {
         if (error) return res.status(400).json({ msg: error.details[0].message });
 
         // Check if the room exists
-        chambre.findAll({ where: { codeChambre: id } })
+        chambre.findOne({ where: { codeChambre: id } })
             .then((response) => {
                 // If the room does not exist, return a 400 Bad Request response
                 if (response.length == 0) return res.status(400).send("La chambre n'existe pas");
                 else {
+                    let data
                     // Create the image URL for storage
-                    const imageUrl = "http://localhost/db/images/" + req.body.imageUrl;
-                    // Check if an image is uploaded
-                    if (!imageBuffer) {
-                        // If no image is uploaded, update room information without changing the image
-                        const data = {
-                            "roomType": req.body.roomType,
-                            "roomDescription": req.body.roomDescription,
-                            "nightlyPrice": req.body.nightlyPrice,
-                            "numberOfPersons": req.body.numberOfPersons,
-                            "amenities": req.body.amenities,
-                            "status": req.body.status,
-                        };
-                        // Update the room
-                        chambre.update({ ...data }, { where: { codeChambre: id } })
-                            .then(response => {
-                                const msg = "Chambre modifiée avec succès";
-                                res.status(201).json({ msg, response });
-                            })
-                            .catch(error => res.json(error));
-                    } else {
-                        // If an image is uploaded, update room information and change the image
-                        const data = {
+                    if (req.body.imageUrl) {
+                        const imageUrl = "http://localhost/db/images/" + req.body.imageUrl;
+                        data = {
                             "roomType": req.body.roomType,
                             "roomDescription": req.body.roomDescription,
                             "nightlyPrice": req.body.nightlyPrice,
@@ -81,14 +49,30 @@ module.exports = (app) => {
                             "status": req.body.status,
                             "imageUrl": imageUrl
                         };
-                        // Update the room
-                        chambre.update({ ...data }, { where: { id: id } })
-                            .then(response => {
+                    } else {
+                        data = {
+                            "roomType": req.body.roomType,
+                            "roomDescription": req.body.roomDescription,
+                            "nightlyPrice": req.body.nightlyPrice,
+                            "numberOfPersons": req.body.numberOfPersons,
+                            "amenities": req.body.amenities,
+                            "status": req.body.status,
+                        };
+                    }
+
+                    // If an image is uploaded, update room information and change the image
+
+                    // Update the room
+                    chambre.update({ ...data }, { where: { codeChambre: id } })
+                        .then(response => {
+                            if (response) {
                                 const msg = "Chambre modifiée avec succès";
                                 res.status(201).json({ msg, response });
-                            })
-                            .catch(error => res.json(error));
-                    }
+                            }
+
+                        })
+                        .catch(error => res.json(error));
+
                 }
             });
     });
